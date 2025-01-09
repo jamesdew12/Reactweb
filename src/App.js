e6a73a69c60da8445ab0149f56c857606534bb95
jamesdew12 <james.dewinton@gmail.com>
2025-01-09 18:29:19 +0000

    balls part 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [balls, setBalls] = useState(
+    Array.from({ length: 100 }, (_, i) => ({
+      id: i,
+      hovered: false,
+    }))
+  );
+
+  const handleHover = (id) => {
+    setBalls((prevBalls) =>
+      prevBalls.map((ball) =>
+        ball.id === id ? { ...ball, hovered: true } : ball
+      )
+    );
+  };
+
+  const handleHoverOut = (id) => {
+    setBalls((prevBalls) =>
+      prevBalls.map((ball) =>
+        ball.id === id ? { ...ball, hovered: false } : ball
+      )
+    );
+  };
+
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <div className="ball-container">
+        {balls.map((ball) => (
+          <div
+            key={ball.id}
+            className={`ball ${ball.hovered ? "hovered" : ""}`}
+            onMouseEnter={() => handleHover(ball.id)}
+            onMouseLeave={() => handleHoverOut(ball.id)}
+          ></div>
+        ))}
+      </div>
     </div>
   );
-}
-
+};
 export default App;
