442d21ccc71eb6845806ec75b4b542cf1b6e9723
jamesdew12 <james.dewinton@gmail.com>
2025-01-09 18:35:58 +0000

    lots of balls but quicker, need more

--- src/App.js
+++ src/App.js
@@ -1,42 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 
 const App = () => {
-  // Generate a grid of balls (dynamic to fill the screen)
-  const ballCount = 1500; // Adjust the number of balls if necessary
-  const [balls, setBalls] = useState(
-    Array.from({ length: ballCount }, (_, i) => ({
-      id: i,
-      hovered: false,
-    }))
-  );
-
-  const handleHover = (id) => {
-    setBalls((prevBalls) =>
-      prevBalls.map((ball) =>
-        ball.id === id ? { ...ball, hovered: true } : ball
-      )
-    );
-  };
-
-  const handleHoverOut = (id) => {
-    setBalls((prevBalls) =>
-      prevBalls.map((ball) =>
-        ball.id === id ? { ...ball, hovered: false } : ball
-      )
-    );
-  };
+  const balls = Array.from({ length: 10000 }); // Generate a large number of balls
 
   return (
     <div className="App">
       <div className="ball-container">
-        {balls.map((ball) => (
-          <div
-            key={ball.id}
-            className={`ball ${ball.hovered ? "hovered" : ""}`}
-            onMouseEnter={() => handleHover(ball.id)}
-            onMouseLeave={() => handleHoverOut(ball.id)}
-          ></div>
+        {balls.map((_, i) => (
+          <div key={i} className="ball"></div>
         ))}
       </div>
     </div>
